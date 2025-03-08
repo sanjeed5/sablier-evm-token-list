cfb5337b81ccc80a31b62155f029bbd3268fcee9
Paul Razvan Berg <prberg@proton.me>
2025-03-07 18:05:49 -0600

    build: switch to github pages
    ci: explanatory comments everywhere
    ci: rename workflows and jobs
    docs: explanatory comments in write.js
    refactor: update build file name

--- src/write.js
+++ src/write.js
@@ -1,3 +1,16 @@
+// Import the buildList function from a local module
 const buildList = require("./buildList");
 
-buildList().then((data) => console.log(JSON.stringify(data, null, 2)));
+// Call the buildList function which returns a Promise
+buildList()
+  .then((data) => {
+    // Format the data as a pretty-printed JSON string with 2-space indentation
+    const formattedData = JSON.stringify(data, null, 2);
+
+    // Output the formatted data to the console
+    console.log(formattedData);
+  })
+  .catch((error) => {
+    // Deal with potential failures
+    console.error("Failed to build list:", error);
+  });
